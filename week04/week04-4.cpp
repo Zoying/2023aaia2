9ce5765ad290f51ab4ad434fcfca267a7269b694
Zoying <144758481+Zoying@users.noreply.github.com>
2024-03-13 11:55:43 +0800

    week04

---
+++ week04/week04-4.cpp
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+  int a = 3;
+  while (a > 0)
+  {
+    printf("a¬O%d\n", a);
+    a --; /// a-=1 ªºÁY¼g
+  }
+  for (int b = 3; b > 0; b --)
+  {
+      printf("b¬O%d\n", b);
+  }
+}
